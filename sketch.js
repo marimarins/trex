187b32d1e6d6c772e7d3fca9cd537938e6894be7
marimarins <95651982+marimarins@users.noreply.github.com>
2022-02-14 16:42:08 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -32,15 +32,16 @@ function preload(){
   scoresom = loadSound ("checkpoint.mp3")
   jumpsom = loadSound ("jump.mp3")
   mortesom = loadSound ("die.mp3")
- // trex_collided = loadAnimation ("trex_collided.png")
+  trex_collided = loadAnimation ("trex_collided.png")
 }
 
 function setup(){
-  createCanvas(600,200);
+  createCanvas(windowWidth,windowHeight);
   
   //criando o trex
-  trex = createSprite(50,160,20,50);
+  trex = createSprite(50,height-70,20,50);
   trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided", trex_collided)
   edges = createEdgeSprites();
   
   //adicione dimensão e posição ao trex
@@ -48,20 +49,20 @@ function setup(){
   trex.x = 25
 
   //criando o solo 
-  solo = createSprite (200, 180, 600, 20);
+  solo = createSprite (width/2, height-80, width, 20);
   solo.addImage (soloing);
 
   //criando o solo invisivel 
-  soloinvisivel = createSprite (200, 190, 600, 20);
+  soloinvisivel = createSprite (width/2, height-70, width, 20);
   soloinvisivel.visible = false;
 
   grupinho= new Group ();
   grupinhozinho = new Group ();
  trex.setCollider("rectangle", 0,0, 40, trex.height);
-  trex.debug= true;
+ 
 
-restart= createSprite (300,140, 10,10)
-gameover= createSprite (300, 100, 10 ,10)
+restart= createSprite (width/2, height/2, 10,10)
+gameover= createSprite (width/2, height/2-50, 10 ,10)
 gameover.addImage(gameoverimg)
 restart.addImage(restartimg)
 restart.scale=0.5
@@ -72,13 +73,12 @@ gameover.scale=2
 function draw(){
   //definir a cor do plano de fundo 
   background("white");
-  console.log(estadojogo);
+  
 text ("pontuação: " + pontuacao, 40, 40);
-
 
 if (estadojogo===PLAY){
 
-  pontuacao = pontuacao + Math.round(frameCount/60);
+  pontuacao = pontuacao + Math.round(getFrameRate()/60);
 if (pontuacao > 0 && pontuacao %100 ===0){
  scoresom.play();
 }
@@ -89,9 +89,10 @@ if (solo.x < 0 ) {
 }
 
  //pular quando tecla de espaço for pressionada
- if(keyDown("space")&& trex.y >= 100){
+ if((touches.length>0||keyDown("space"))&& trex.y >= height-120){
   trex.velocityY = -10;
   jumpsom.play();
+  touches=[];
 }
 
 //gravidade do trex
@@ -112,12 +113,17 @@ else if (estadojogo===END){
 solo.velocityX=0
 grupinho.setVelocityXEach(0);
 grupinhozinho.setVelocityXEach(0)
-//trex.changeAnimation ("collided", trex_collided)
+trex.changeAnimation ("collided", trex_collided)
 trex.velocityY=0
 grupinhozinho.setLifetimeEach(-1)
 grupinho.setLifetimeEach (-1)
 gameover.visible= true; 
  restart.visible=true;
+ if (touches.length>0||mousePressedOver(restart)){
+  reset()
+  touches=[];
+ 
+ }
 }
  
   //registrando a posição y do trex
@@ -127,6 +133,7 @@ gameover.visible= true;
   
  //impedir que o trex caia
   trex.collide(soloinvisivel);
+
 
  
   drawSprites();
@@ -139,12 +146,12 @@ function gerarnuvens(){
 
   
 
-nuvens=createSprite (600,100, 40, 10);
+nuvens=createSprite (width+29, height-300, 40, 10);
 nuvens.addImage(nuvensing);
 nuvens.velocityX = -2 ;
-nuvens.scale = 0.4;
-nuvens.y = Math.round(random(10, 60));
-nuvens.lifetime = 300;
+nuvens.scale = 0.9;
+nuvens.y = Math.round(random(100, 220));
+nuvens.lifetime = width/nuvens.velocityX;
 trex.depth=nuvens.depth;
 trex.depth=trex.depth+1;
 grupinho.add(nuvens)
@@ -153,7 +160,7 @@ grupinho.add(nuvens)
 function gerarobstaculos (){
 if (frameCount%80===0){
 
-  obstaculos=createSprite (600, 165, 10, 40);
+  obstaculos=createSprite (width+29, height-95, 10, 40);
   obstaculos.velocityX = -(4+pontuacao/ 100);
   var rand= Math.round(random(1, 6));
   switch (rand) {
@@ -171,7 +178,15 @@ if (frameCount%80===0){
     break;
   }
   obstaculos.scale = 0.5;
-  obstaculos.lifetime = 170;
-  grupinhozinho.add (obstaculos)
+  obstaculos.lifetime = width/obstaculos.velocityX;
+  grupinhozinho.add (obstaculos);
 }
+}
+function reset (){
+ estadojogo= PLAY;
+ grupinhozinho.destroyEach() ;          
+ grupinho.destroyEach();
+ pontuacao=0;
+ trex.changeAnimation("running", trex_running)
+
 }
